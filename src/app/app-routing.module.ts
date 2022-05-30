1e0e37d2e40a21ae0b5e217cbc32a2964eed2cbb
ester-ferr <estereliege_ferr@hotmail.com>
2022-05-30 11:34:58 -0300

    criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
+
   {path:'', redirectTo: 'entrar', pathMatch:'full'},
+
   {path:'entrar', component: EntrarComponent},
   {path:'cadastrar', component: CadastrarComponent}
+
 ];
 
 @NgModule({
