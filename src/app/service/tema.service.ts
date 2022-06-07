f180361a8c9865044cac4d5643c8f2da02dfffb7
ester-ferr <estereliege_ferr@hotmail.com>
2022-06-07 18:01:04 -0300

    implementando put e delete do tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,7 +19,19 @@ export class TemaService {
     return this.http.get<Tema[]>('https://esterzinhablogpessoall.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://esterzinhablogpessoall.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://esterzinhablogpessoall.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://esterzinhablogpessoall.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://esterzinhablogpessoall.herokuapp.com/tema/${id}`, this.token)
   }
 }
