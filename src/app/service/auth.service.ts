9bd575d4ea98e77fae8b5f4d7b3b9830f589b036
ester-ferr <estereliege_ferr@hotmail.com>
2022-06-08 20:38:40 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -20,6 +20,11 @@ export class AuthService {
 
   cadastrar(user: User): Observable <User> {
     return this.http.post <User> ('https://esterzinhablogpessoall.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://esterzinhablogpessoall.herokuapp.com/usuarios/${id}`,
+    {headers: new HttpHeaders().set('Authorization', environment.token)})
   }
 
   logado() {
