fdf3909e852b7e12b7da75362bb51a5f542dc293
ester-ferr <estereliege_ferr@hotmail.com>
2022-06-13 16:54:22 -0300

    implementando put 'atualizar cadastro' do user

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,9 +11,8 @@ export class MenuComponent implements OnInit {
 
   nome = environment.nome
   foto = environment.foto
+  id = environment.id
   
-
-
   constructor(
     private router: Router
   ) { }
